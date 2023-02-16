df02f76295b4ded6baea1780fb36e0ef9f1bd74a
LuanSilva669 <125572605+LuanSilva669@users.noreply.github.com>
2023-02-16 11:19:31 -0300

    Mudei umas parada ae

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -3,7 +3,17 @@ import './style.css'
 function Banner() {
     return (
         <div className="banner">
-            Eu sou um banner !
+            <img className='logo' src="https://cf.shopee.com.br/file/5f5874e31a74551ea8c503580f58ba44" alt="" />
+            <div className="Box">
+                Eu sou um banner 2
+            </div>
+            <div className="Box">
+                Eu sou um banner 2
+            </div>
+            <div className="Box">
+                Eu sou um banner 2
+            </div>
+          
         </div>
     )
 }
