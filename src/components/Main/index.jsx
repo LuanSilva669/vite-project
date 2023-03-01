43455786ce2193b43b69d930bdbd127b0345d432
eduardo <eduardo@uizzy.com.br>
2023-03-01 16:45:11 -0300

    feat: Added sample of main :D

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -4,19 +4,35 @@ import { Column, Container, Information, Line, Image, Title, Description } from 
 function Main() {
 
     return (
-        <Container>
-            <Line />
-            <Information>
-                <Image src="/logo.png" alt="logo" />
-                <Column>
-                    <Title>Sobre a marca</Title>
-                    <Description>
-                        Bats in Black é uma marca de moda alternativa que celebra a beleza sombria e misteriosa da noite. Com a imagem icônica do morcego como seu símbolo, traz um toque único e elegante onde cada peça é projetada com atenção aos detalhes e com um estilo distinto. Moda alternativa que voa alto entregando a beleza sombria das asasinhas de um morcego.
-                    </Description>
-                </Column>
-            </Information>
-            <Line className="reverse" />
-        </Container>
+        <>
+            <Container>
+                <Line />
+                <Information>
+                    <Image src="/logo.png" alt="logo" />
+                    <Column>
+                        <Title>Sobre a marca</Title>
+                        <Description>
+                            Bats in Black é uma marca de moda alternativa que celebra a beleza sombria e misteriosa da noite. Com a imagem icônica do morcego como seu símbolo, traz um toque único e elegante onde cada peça é projetada com atenção aos detalhes e com um estilo distinto. Moda alternativa que voa alto entregando a beleza sombria das asasinhas de um morcego.
+                        </Description>
+                    </Column>
+                </Information>
+                <Line className="reverse" />
+            </Container>
+
+            <Container>
+                <Line />
+                <Information>
+                    <Image src="/logo.png" alt="logo" />
+                    <Column>
+                        <Title>Gand</Title>
+                        <Description>
+                            Gand Gand Gand Gand Gand Gand Gand Gand Gand Gand Gand Gand Gand Gand Gand Gand Gand Gand
+                        </Description>
+                    </Column>
+                </Information>
+                <Line className="reverse" />
+            </Container>
+        </>
     )
 
 }
