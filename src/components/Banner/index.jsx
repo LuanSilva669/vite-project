1b0c52182659d29dc5d9c4dea2ad439de3cbb604
LuanSilva669 <125572605+LuanSilva669@users.noreply.github.com>
2023-03-01 11:31:32 -0300

    alterações

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -2,79 +2,70 @@ import React from 'react';
 import './style.css'
 import Slider from 'react-slick';
 
-function Banner() {
+function Carrossel() {
   const bannerInfo = [ 
     {
-    id: 1,
-    image: "/pic2.png",
-    title: "Draculaura",
-    description: "EXIBA SUA ESSÊNCIA GÓTICA EM CADA PASSO.",
-    button: "COMPRE AGORA"
-  },
-  {
-    id: 1,
-    image: "/pic3.png",
-    title: "Draculaura",
-    description: "EXIBA SUA ESSÊNCIA GÓTICA EM CADA PASSO.",
-    button: "COMPRE AGORA"
-  },
+      id: 1,
+      image: "/pic2.png",
+      title: "Draculaura",
+      description: "EXIBA SUA ESSÊNCIA GÓTICA EM CADA PASSO.",
+      button: "COMPRE AGORA"
+    },
+    {
+      id: 2,
+      image: "/pic3.png",
+      title: "Luna Blue",
+      description: "Cada passo é uma oportunidade para expressar sua autenticidade gótica",
+      button: "COMPRE AGORA"
+    },
+    {
+        id: 3,
+        image: "/picc4.png",
+        title: "Spectra",
+        description: "Expresse sua individualidade com a elegância sombria",
+        button: "COMPRE AGORA"
+      },
   ];
 
-
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 5000
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 5000
   };
-
 
   return (
     <Slider {...settings}>
       {bannerInfo.map((banner) => (
-
-
-<div className="banner" >
-      
-      <img class="bota" src="/pic2.png" alt="Bates-in-Black" title="Bates-in-Black"/>
-      <div class="content">
-    <div className="container1">
-       <title className='draculaura'>
-          Draculaura
-      </title>
-    
+  <div className={`banner ${banner.bannerID}`}>
+    <img className="bota" src={banner.image}  />
+    <div className="content">
+      <div className="container1">
+        <h1 className={banner.title === "Luna Blue" ? "title2" : ""}>
+          {banner.title}
+        </h1>
+      </div>
+      <div className="container3">
+        <h1 className={`text ${banner.title === "Luna Blue" ? "luna-blue-text" : ""}`}>
+          {banner.description}
+        </h1>
+      </div>
+      <div className="botao">
+        <button className="buttonn" href="/Entre em contato">
+          {banner.button}
+        </button> 
+      </div>
     </div>
-      
-      
-<div className='container2'>
-      <h1 className='text'>
-          EXIBA SUA ESSÊNCIA GÓTICA EM CADA PASSO.
-      </h1>
-</div>
-       <div className='botao'>
-         <button className="buttonn"  href="/Entre em contato">
-                COMPRE AGORA
-         </button> 
-        </div>
-</div>
   </div>
+))}
+    </Slider>
+  )
+}
 
-          ))}
-          </Slider>
-        );
-      }
-
-
-export default Banner;
-
-
-
-
-
-
+export default Carrossel;
 
 
 
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -10,17 +10,17 @@ function Navbar() {
                 <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/d26e2053-1559-4c05-99ef-44c42cfe87f7/cf47107b-8df8-4929-89bb-a251ac9fa0aa?org_if_sml=1&h=200" height="150px"  alt="My Logo" />
               </div>
               <div className="navbar-menu">
-                <a className="navbar-item"  href="/Inicio" style={{ marginLeft:"90px",color: 'white', fontFamily: 'roboto', fontSize: '20px', textDecoration: 'none', }}>
+                <a className="navbar-item"  href="/Inicio" >
                   INICIO
                 </a>
-                <a className="navbar-item" href="/sobre a marca"style={{marginRight:"100px", color: 'white', fontFamily: 'roboto', fontSize: '20px',textDecoration: 'none', }}>
+                <a className="navbar-item" href="/sobre a marca">
                  SOBRE A MARCA
                 </a>
-                <a className="navbar-item" href="/sobre a coleção"style={{marginRight:"70px",color: 'white', fontFamily: 'Roboto',fontSize: '20px',textDecoration: 'none', }}>
+                <a className="navbar-item" >
                 SOBRE A COLEÇÃO
                 </a>
               </div>
-              <button className="button"style={{ color: 'blue', fontFamily: 'roboto', fontSize:'14Px',textDecoration: 'underline'  }} href="/Entre em contato">
+              <button className="button" href="/Entre em contato">
                 ENTRE EM CONTATO 
               </button>
    
