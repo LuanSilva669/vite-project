fca22535e1fb6dbca36d5a29261ab8fd93f6b80d
LuanSilva669 <125572605+LuanSilva669@users.noreply.github.com>
2023-05-25 14:59:21 -0300

    correções

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import  './style.css';
+import './style.css';
 
 function Footer() {
-
-    return (
-        <div className="footer" id='rodape'>
-            <img src="2.png" alt="" />
-            <img src="3.png" alt="" />
-            <img src="4.png" alt="" />
-            <img src="5.png" alt="" />
-            </div>
-    )
+  return (
+    <div className="footer" id="rodape">
+      <a href="https://twitter.com/home" target="_blank" rel="noopener noreferrer">
+        <i className="fab fa-twitter"></i>
+      </a>
+      <a href="https://www.whatsapp.com" target="_blank" rel="noopener noreferrer">
+        <i className="fab fa-whatsapp"></i>
+      </a>
+      <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+        <i className="fab fa-instagram"></i>
+      </a>
+      <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+        <i className="fab fa-facebook"></i>
+      </a>
+    </div>
+  );
 }
 
 export default Footer;
